451eec4a2743b607bc6cb00d4d5b3a50c506137f
Nick Heiner <nth23@cornell.edu>
2016-10-22 19:14:05 -0400

    react-bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ProgressBar } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.less';
 
@@ -6,11 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="progress">
-          <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: '60%'}}>
-            <span className="sr-only">60% Complete</span>
-          </div>
-        </div>
+        <ProgressBar bsStyle="success" now={40} />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>savage af</h2>
