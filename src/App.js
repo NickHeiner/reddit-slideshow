811b882a57aff0636fb6a55ddeda4a701dcbf2be
Nick Heiner <nth23@cornell.edu>
2016-10-22 19:08:27 -0400

    Bootstrap static elements are working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="progress">
+          <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: '60%'}}>
+            <span className="sr-only">60% Complete</span>
+          </div>
+        </div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>savage af</h2>
