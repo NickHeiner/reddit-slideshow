3edd5b69ee08abf8c853ab28eca9c19c116cbf4c
Nick Heiner <nth23@cornell.edu>
2016-10-23 10:24:12 -0400

    Adding a settings button that does not do anything yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Glyphicon } from 'react-bootstrap';
 import PhotoFrame from './PhotoFrame';
 import './App.less';
 
 class App extends Component {
   render() {
     return (
-      <div className="grid-base">
+      <div className="base">
+        <Glyphicon glyph="cog" className="menu-trigger" role="button" aria-label="Open settings" style={
+          {
+            fontSize: '5rem',
+            position: 'absolute',
+            padding: '10px',
+            top: 'initial',
+            bottom: '0px',
+            left: '0px'
+          }
+        } />
         <PhotoFrame />
       </div>
     );
